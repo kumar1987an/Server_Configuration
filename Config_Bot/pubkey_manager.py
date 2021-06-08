c60b974be9bb77ebc8852814bdf4ef740da1c6c4
kumar1987an <kumar1987an@gmail.com>
2021-06-08 09:53:10 -0700

    .

--- Config_Bot/pubkey_manager.py
+++ Config_Bot/pubkey_manager.py
@@ -42,19 +42,11 @@ class Pubkey:
     def path_creator(user):
 
         logger.warning(
-            "pubkey store directory for user {} doesn't exist, creating it now".format(user))
+            " Pubkey store directory for user {} doesn't exist, creating it now".format(user))
         os.makedirs("/SSH_Keys/{}".format(user))
         if os.path.lexists("/SSH_Keys"):
             logger.info(
-                "pubkey store directory for user {} created printing output".format(user))
-
-    @staticmethod
-    def hash_checker(file):
-        md5_hash = hashlib.md5()
-        with open(file, "rb") as hash_file:
-            content = hash_file.read()
-        md5_hash.update(content)
-        return md5_hash.hexdigest()
+                " Pubkey store directory for user {} created Success".format(user))
 
     @staticmethod
     def authorized_keys(user, pub_key):
@@ -63,12 +55,11 @@ class Pubkey:
             path = "/SSH_Keys/{}".format(user)
             authorized_file = os.path.join(path, "authorized_keys2")
             Filecopy.backup("{}".format(authorized_file), Type="secured")
-            # print(Filecopy.backup.__dict__["new_file_name"])
             try:
                 with open(authorized_file, "a") as key_file:
                     key_file.write("{}\n".format(pub_key))
                 logger.info(
-                    "Updated authorization file for user {}".format(user))
+                    " Updated authorization file for user {}".format(user))
 
             except Exception as e:
                 print(e)
