53191cb1c62cad1dab260f1880ddac3dc30329d0
kumar1987an <kumar1987an@gmail.com>
2021-10-18 08:08:06 -0700

    .

--- Config_Bot/checker.py
+++ Config_Bot/checker.py
@@ -11,6 +11,7 @@
 # import os
 import logging
 import re
+
 # from subprocess import PIPE, call, Popen
 
 # Importing required libraries
@@ -29,11 +30,13 @@ logger.addHandler(stream_handler)
 
 
 class Duplicate(object):
+    """Checking for ducplication of configuration on a file"""
 
     @staticmethod
     def single_pattern_file_checker(single_pattern, file):
+        """Single pattern file checker"""
         check_for = re.compile(r"{}".format(single_pattern))
-        with open(file, "r") as check_file:
+        with open(file, "r", encoding="utf-8") as check_file:
             file_data = check_file.read()
             pattern_check = re.search(check_for, file_data)
             if pattern_check is None:
@@ -43,17 +46,17 @@ class Duplicate(object):
 
     @staticmethod
     def multi_pattern_file_checker(multi_pattern, file):
+        """Multiple pattern file checker"""
         list_of_pattern_verified = []
         for pattern in multi_pattern:
             check_for = re.compile(r"{}".format(pattern))
-            with open(file, "r") as check_file:
+            with open(file, "r", encoding="utf-8") as check_file:
                 file_data = check_file.read()
                 pattern_check = re.search(check_for, file_data)
                 if pattern_check is None:
                     list_of_pattern_verified.append(True)  # Execute the Script
                 else:
-                    list_of_pattern_verified.append(
-                        False)  # Don't Execute the Script
+                    list_of_pattern_verified.append(False)  # Don't Execute the Script
 
         if all(list_of_pattern_verified):
             return 1  # Execute the Script
